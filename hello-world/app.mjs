830bfdd4e9c840fefaf611eb2349ccf4cd4da1fd
jcardus <asklocation.net@gmail.com>
2024-01-25 03:50:59 +0000

    finally working

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -1,30 +1,22 @@
 import AmazonCognitoIdentity  from 'amazon-cognito-identity-js'
 import axios from 'axios'
-
-export const lambdaHandler = async (e, context) => {
-    console.log(e)
+export const lambdaHandler = async (e) => {
     const poolData = {
         UserPoolId: 'us-east-1_SWTiH7d38',
         ClientId: '16seahimlsre6ocin0uvivtet2'
     }
-
     const Pool = new AmazonCognitoIdentity.CognitoUserPool(poolData)
-
     const Username = e.userName
     const Password = e.request.password
     const authenticationData = { Username, Password }
     const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData)
     const userData = { Username, Pool }
-
     const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData)
     console.log(await new Promise((resolve, reject) => {
         cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: () => {
-                console.log('auth ok cognito', e.userName)
-                resolve('Authentication successful')
-            },
+            onSuccess: () => { resolve('auth ok cognito', e.userName)},
             onFailure: (err) => {
-                console.log('failure, lets try traccar', err)
+                console.log(e.userName, 'failed:', err.message, 'lets try traccar')
                 const body = 'email=' + encodeURIComponent(Username) + '&password=' + encodeURIComponent(Password)
                 console.log(body)
                 axios.post('https://api2.pinme.io/api/session', body, {
@@ -35,19 +27,16 @@ export const lambdaHandler = async (e, context) => {
                 }).then(r => resolve(r.data)).catch(e => reject(e))
             },
             newPasswordRequired: () => {
-                console.log('new password required', e.userName)
+                console.log('new password required', Username)
                 resolve('New password required')
             }
         })
     }))
-
     e.response.userAttributes = {
+        username: Username,
         email: Username,
-        email_verified: 'true',
-        username: Username
+        email_verified: true
     }
     e.response.finalUserStatus = 'CONFIRMED'
-    context.succeed(e)
-    console.log('returning', e)
     return e
 }
